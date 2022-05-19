ad1abb026aa4f2b1eab6f310463b25f42d0fa2af
devrayat000 <2010050@me.buet.ac.bd>
2022-05-19 21:21:02 +0600

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,39 @@
+<div class="prose max-w-none p-6 prose-h1:mb-0">
+	<header class="flex justify-between items-center">
+		<section>
+			<h1>Shortly</h1>
+		</section>
+		<nav>
+			<button id="burger" class="flex flex-col items-stretch justify-center gap-1">
+				<span class="h-1 w-7 bg-gray-400 rounded-lg" />
+				<span class="h-1 w-7 bg-gray-400 rounded-lg" />
+				<span class="h-1 w-7 bg-gray-400 rounded-lg" />
+			</button>
+		</nav>
+	</header>
+
+	<article>
+		<header>
+			<img src="/images/illustration-working.svg" alt="Hero" />
+		</header>
+		<main class="prose-h2:text-center prose-p:text-center">
+			<h2>More than just shorter link</h2>
+			<p>
+				Build your brand's recognition and get detailed insights on how your links are performing.
+			</p>
+			<div class="grid place-items-center">
+				<button class="bg-cyan-500 text-white rounded-full px-6 py-2 font-medium"
+					>Get Started</button
+				>
+			</div>
+		</main>
+	</article>
+
+	<main class="relative bg-purple-900 p-5 rounded-md overflow-hidden my-10">
+		<img src="/images/bg-shorten-mobile.svg" alt="Shorten" class="absolute right-0 top-0 m-0" />
+		<section class="relative flex flex-col items-stretch gap-4">
+			<input type="text" class="rounded py-2 px-4" placeholder="Shorten a link here..." />
+			<button class="bg-cyan-500 text-white rounded px-6 py-2 font-medium">Shorten It!</button>
+		</section>
+	</main>
+</div>
